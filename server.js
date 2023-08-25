23c743f023be16f4f608245cb9d4bc9d12d711d6
eswarprasadk <eswarprasadk@turito.com>
2023-08-25 14:26:56 +0530

    cookie session authentication

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const cookieSession = require("cookie-session");
 const express = require("express");
 const fs = require("fs");
 const helmet = require("helmet");
@@ -10,12 +11,17 @@ require("dotenv").config();
 const config = {
   CLIENT_ID: process.env.CLIENT_ID,
   CLIENT_SECRET: process.env.CLIENT_SECRET,
+  COOKIE_KEY_1: process.env.COOKIE_KEY_1,
+  COOKIE_KEY_2: process.env.COOKIE_KEY_2,
 };
 const AUTH_OPTIONS = {
   callbackURL: "/auth/google/callback",
   clientID: config.CLIENT_ID,
   clientSecret: config.CLIENT_SECRET,
 };
+
+passport.serializeUser((user, done) => done(null, user.id));
+passport.deserializeUser((obj, done) => done(null, obj));
 
 function verifyCallback(accessToken, refreshToken, profile, done) {
   console.log("Google profile", profile);
@@ -26,10 +32,19 @@ passport.use(new Strategy(AUTH_OPTIONS, verifyCallback));
 const app = express();
 
 app.use(helmet());
+
+app.use(
+  cookieSession({
+    name: "session",
+    maxAge: 24 * 60 * 60 * 1000,
+    keys: [config.COOKIE_KEY_1, config.COOKIE_KEY_2],
+  })
+);
 app.use(passport.initialize());
+app.use(passport.session());
 
 function checkLoggedIn(req, res, next) {
-  const isLoggedIn = true;
+  const isLoggedIn = req.isAuthenticated() && req.user;
   !isLoggedIn ? res.status(401).json({ error: "Please login" }) : next();
 }
 
@@ -44,14 +59,17 @@ app.get(
   passport.authenticate("google", {
     failureRedirect: "/auth/failure",
     successRedirect: "/",
-    session: false,
+    session: true,
   }),
   (req, res) => console.log("google called us")
 );
 app.get("/auth/failure", (req, res) =>
   res.status(401).send("Failed to authorize")
 );
-app.get("/auth/logout", (req, res) => {});
+app.get("/auth/logout", (req, res) => {
+  req.logout();
+  res.redirect("/");
+});
 
 app.get("/secret", checkLoggedIn, (req, res) => {
   return res.send("Your personal secret value is 42!");
