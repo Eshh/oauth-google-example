3022023c91e4a166a7b245841a2dd00f3ade7809
eswarprasadk <eswarprasadk@turito.com>
2023-08-25 12:51:13 +0530

    google oauth code with passport

--- server.js
+++ server.js
@@ -2,23 +2,55 @@ const express = require("express");
 const fs = require("fs");
 const helmet = require("helmet");
 const https = require("https");
+const passport = require("passport");
+const { Strategy } = require("passport-google-oauth20");
 const path = require("path");
 require("dotenv").config();
 
-const app = express();
 const config = {
   CLIENT_ID: process.env.CLIENT_ID,
   CLIENT_SECRET: process.env.CLIENT_SECRET,
 };
+const AUTH_OPTIONS = {
+  callbackURL: "/auth/google/callback",
+  clientID: config.CLIENT_ID,
+  clientSecret: config.CLIENT_SECRET,
+};
+
+function verifyCallback(accessToken, refreshToken, profile, done) {
+  console.log("Google profile", profile);
+  done(null, profile);
+}
+passport.use(new Strategy(AUTH_OPTIONS, verifyCallback));
+
+const app = express();
+
 app.use(helmet());
+app.use(passport.initialize());
 
 function checkLoggedIn(req, res, next) {
   const isLoggedIn = true;
   !isLoggedIn ? res.status(401).json({ error: "Please login" }) : next();
 }
 
-app.get("/auth/google", (req, res) => {});
-app.get("/auth/google/callback", (req, res) => {});
+app.get(
+  "/auth/google",
+  passport.authenticate("google", {
+    scope: ["email", "profile"],
+  })
+);
+app.get(
+  "/auth/google/callback",
+  passport.authenticate("google", {
+    failureRedirect: "/auth/failure",
+    successRedirect: "/",
+    session: false,
+  }),
+  (req, res) => console.log("google called us")
+);
+app.get("/auth/failure", (req, res) =>
+  res.status(401).send("Failed to authorize")
+);
 app.get("/auth/logout", (req, res) => {});
 
 app.get("/secret", checkLoggedIn, (req, res) => {
